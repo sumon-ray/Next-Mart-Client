3ff9baf3ed56e8bb31401460daf0632c6e4f4a02
sumon-ray <raysumon220020@gmail.com>
2025-03-13 09:22:31 +0600

    sidebar added

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import { AppSidebar } from "@/components/module/sidebar/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import * as React from 'react';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
-const DashboardLayout = ({children}: {children: React.ReactNode}) => {
-    return (
-        <div>
-            {children}
+export default function DashboardLayout({children}: {children:React.ReactNode}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+    
+          {children}
         </div>
-    );
-};
-
-export default DashboardLayout;
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
